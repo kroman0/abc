6f0442644b711cf766ad21663e2adb3a00053e36
Roman Kozlovskyi <krzroman@gmail.com>
2013-09-07 14:14:40 +0300

    updated AngularJS

--- app/static/js/angular-resource.js
+++ app/static/js/angular-resource.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.0-e12e584
+ * @license AngularJS v1.2.0-4def730
  * (c) 2010-2012 Google, Inc. http://angularjs.org
  * License: MIT
  */
--- app/static/js/angular.js
+++ app/static/js/angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.0-e12e584
+ * @license AngularJS v1.2.0-4def730
  * (c) 2010-2012 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1556,7 +1556,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.0-e12e584',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.0-4def730',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 0,
